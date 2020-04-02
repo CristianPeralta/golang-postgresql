8ad1da27111b89b9a802c1dfac2c65d0c9b8683b
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-02 12:57:33 -0500

    log if connection and disconnecttion is succesful

--- db/main.go
+++ db/main.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"log"
+	"os"
+
 	pg "github.com/go-pg/pg"
 )
 
@@ -11,5 +14,16 @@ func Connect() {
 		Addr:     "localhost:5432",
 	}
 	var db *pg.DB = pg.Connect(opts)
+	if db == nil {
+		log.Printf("Failed to connect to database.\n")
+		os.Exit(100)
+	}
+	log.Printf("Connecttion to database Successful.\n")
+	closeErr := db.Close()
+	if closeErr != nil {
+		log.Printf("Error while closing the connection, Reason: %v\n", closeErr)
+		os.Exit(100)
+	}
+	log.Printf("Connecttion closed Successfully.\n")
 	return
 }
