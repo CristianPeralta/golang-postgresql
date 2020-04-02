21f17d5a2aba81840608b11f109572f1deda9276
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-02 16:40:50 -0500

    add SaveProduct method

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"log"
+	"time"
 
 	db "./db"
+	"github.com/go-pg/pg"
 )
 
 func main() {
 	log.Printf("Hello world!")
 	pgDb := db.Connect()
+	SaveProduct(pgDb)
+}
+
+func SaveProduct(dbRef *pg.DB) {
+	newPI := db.ProductItem{
+		Name:  "Product 1",
+		Desc:  "Product 1 Desc",
+		Image: "this is an image path",
+		Price: 3.5,
+		Features: struct {
+			Name string
+			Desc string
+			Imp  int
+		}{
+			Name: "Fi",
+			Desc: "Desc F1",
+			Imp:  3,
+		},
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		IsActive:  true,
+	}
+	newPI.Save(dbRef)
 }
