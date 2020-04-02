b0373df6a5933f4ec229034426fd1ae8e7ecf5ea
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-02 15:17:16 -0500

    update Features prop

--- db/product.go
+++ db/product.go
@@ -3,13 +3,17 @@ package db
 import "time"
 
 type ProductItem struct {
-	tableName struct{}  `sql:"product_items_collection"`
-	ID        int       `sql:"id, pk"`
-	Name      string    `sql:"name:unique"`
-	Desc      string    `sql:"desc"`
-	Image     string    `sql:"image"`
-	Price     float64   `sql:"price, type:real"`
-	Features  string    `sql:"features"`
+	tableName struct{} `sql:"product_items_collection"`
+	ID        int      `sql:"id, pk"`
+	Name      string   `sql:"name:unique"`
+	Desc      string   `sql:"desc"`
+	Image     string   `sql:"image"`
+	Price     float64  `sql:"price, type:real"`
+	Features  struct {
+		Name string
+		Desc string
+		Imp  int
+	} `sql:"features, type:jsonb"`
 	CreatedAt time.Time `sql:"created_at"`
 	UpdatedAt time.Time `sql:"updated_at"`
 	IsActive  bool      `sql:"is_active"`
