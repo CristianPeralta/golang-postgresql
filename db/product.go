c053ef7f3123a38d21b84815700f052321887f82
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-02 15:19:56 -0500

    add RefPointer

--- db/product.go
+++ db/product.go
@@ -3,13 +3,14 @@ package db
 import "time"
 
 type ProductItem struct {
-	tableName struct{} `sql:"product_items_collection"`
-	ID        int      `sql:"id, pk"`
-	Name      string   `sql:"name:unique"`
-	Desc      string   `sql:"desc"`
-	Image     string   `sql:"image"`
-	Price     float64  `sql:"price, type:real"`
-	Features  struct {
+	RefPointer int      `sql:"-"`
+	tableName  struct{} `sql:"product_items_collection"`
+	ID         int      `sql:"id, pk"`
+	Name       string   `sql:"name:unique"`
+	Desc       string   `sql:"desc"`
+	Image      string   `sql:"image"`
+	Price      float64  `sql:"price, type:real"`
+	Features   struct {
 		Name string
 		Desc string
 		Imp  int
