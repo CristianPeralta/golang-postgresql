53f8009607bd4ef4c0dc8ad2ee57803b98f896ee
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-03 11:08:56 -0500

    test PlaceholderDemo with custom struct

--- db/params.go
+++ db/params.go
@@ -6,14 +6,23 @@ import (
 	pg "github.com/go-pg/pg"
 )
 
+type Params struct {
+	Param1 string
+	Param2 string
+}
+
 func PlaceHolderDemo(db *pg.DB) error {
-	var value int
-	var query string = "SELECT ?"
-	_, selectErr := db.Query(pg.Scan(&value), query, 42)
+	var value string
+	var query string = "SELECT ?param1"
+	params := Params{
+		Param1: "This is param 1",
+		Param2: "This is param 2",
+	}
+	_, selectErr := db.Query(pg.Scan(&value), query, params)
 	if selectErr != nil {
 		log.Printf("Error while running the select query, Reason: %v\n", selectErr)
 		return selectErr
 	}
-	log.Printf("Scan successful, scanned value: %v\n", value)
+	log.Printf("Scan successful, scanned value: %s\n", value)
 	return nil
 }
