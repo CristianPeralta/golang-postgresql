9625bbf0590eb794fdf4ecffd33f895c068ec639
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-03 13:07:26 -0500

    update connection options

--- db/main.go
+++ db/main.go
@@ -3,16 +3,23 @@ package db
 import (
 	"log"
 	"os"
+	"time"
 
 	pg "github.com/go-pg/pg"
 )
 
 func Connect() *pg.DB {
 	opts := &pg.Options{
-		User:     "postgres",
-		Password: "123456",
-		Addr:     "localhost:5432",
-		Database: "mydb",
+		User:         "postgres",
+		Password:     "123456",
+		Addr:         "localhost:5432",
+		Database:     "mydb",
+		DialTimeout:  30 * time.Second,
+		ReadTimeout:  1 * time.Minute,
+		WriteTimeout: 1 * time.Minute,
+		IdleTimeout:  30 * time.Minute,
+		MaxConnAge:   1 * time.Minute,
+		PoolSize:     20,
 	}
 	var db *pg.DB = pg.Connect(opts)
 	if db == nil {
